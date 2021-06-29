d29638f3b1dbd8e365ba549872197241ab292f97
MAHI-MALHANI <76902982+MAHI-MALHANI@users.noreply.github.com>
2021-06-29 19:39:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+var bgImg;
+var spaceCraft;
+var spaceCraftImg1, spaceCraftImg2, spaceCraftImg3, spaceCraftImg4;
+var iss, issImg;
+var hasDocked= false;
+
+function preload(){
+bgImg=loadImage("Docking-undocking/spacebg.jpg");
+
+spaceCraftImg1=loadImage("Docking-undocking/spacecraft1.png");
+spaceCraftImg2=loadImage("Docking-undocking/spacecraft2.png");
+spaceCraftImg3=loadImage("Docking-undocking/spacecraft3.png");
+spaceCraftImg4=loadImage("Docking-undocking/spacecraft4.png");
+
+issImg=loadImage("Docking-undocking/iss.png");
+}
+
+function setup() {
+  createCanvas(800,500);
+
+  iss=createSprite(400, 180, 50, 50);
+  iss.addImage(issImg);
+  iss.scale=0.9;
+
+  spaceCraft=createSprite(380, 350, 70, 70);
+  spaceCraft.addImage(spaceCraftImg1);
+
+  spaceCraft.scale=0.3;
+
+}
+
+function draw() {
+  background(bgImg);
+  
+  if(!hasDocked){
+      spaceCraft = createSprite(random(100, 1000), 0, 100, 100);
+
+      if(keyDown(LEFT_ARROW)){
+        spaceCraft.addImage(spaceCraftImg3);
+        spaceCraft.x=spaceCraft.x-5;
+      }
+
+      if(keyDown(RIGHT_ARROW)){
+        spaceCraft.addImage(spaceCraftImg4);
+        spaceCraft.x=spaceCraft.x+5;
+      }
+
+      if(keyDown(DOWN_ARROW)){
+        spaceCraft.addImage(spaceCraftImg2);
+      }
+
+      if(keyDown(UP_ARROW)){
+        spaceCraft.addImage(spaceCraftImg2);
+        spaceCraft.y=spaceCraft.y-5;
+      }
+  }
+
+  if(spaceCraft.x===300 && spaceCraft.y===200){
+    hasDocked=true;
+
+    textSize(25);
+    text("Docking Successful!", 750, 350);
+  }
+
+  drawSprites();
+}
